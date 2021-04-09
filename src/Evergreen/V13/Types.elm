afbd381bb010f509addfd2af6f676229fed87951
Mario Rogic <hello@mario.net.au>
2021-04-09 11:42:32 -0500

    Preparing for v13

---
+++ src/Evergreen/V13/Types.elm
@@ -0,0 +1,41 @@
+module Evergreen.V13.Types exposing (..)
+
+import File
+import Lamdera
+import Set
+
+
+type alias FrontendModel =
+    { counter : Int
+    , clientId : String
+    }
+
+
+type alias BackendModel =
+    { counter : Int
+    , clients : Set.Set Lamdera.ClientId
+    , test : String
+    }
+
+
+type FrontendMsg
+    = Increment
+    | Decrement
+    | ClickedSelectFile
+    | FileSelected File
+    | FNoop
+
+
+type ToBackend
+    = ClientJoin
+    | CounterIncremented
+    | CounterDecremented
+    | Blah
+
+
+type BackendMsg
+    = Noop
+
+
+type ToFrontend
+    = CounterNewValue Int String
